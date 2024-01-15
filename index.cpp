d6b05cbb645447c164fd33a785c5ba0be1e3174b
jaenng <110799323+jaenng@users.noreply.github.com>
2024-01-15 17:40:51 +0800

    Edit index.cpp
    after git branch --set-upstream-to=jorigin/master master

--- index.cpp
+++ index.cpp
@@ -3,5 +3,6 @@
 int main(){
     std::cout<<"hello Git world !"<<std::endl;
     std::cout<<"Editing on GitHub web"<<std::endl;
+    std::cout<<"Edit on GitHub web after git branch --set-upstream-to=jorigin/master master"<<std::endl;
     return 0;
 }
