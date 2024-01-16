0b066ec4116842a49b856e1585f779e0ef659a39
jaen.ng.chai <jaenngch@pglc00022.png.intel.com>
2024-01-16 10:37:58 +0800

    Edit index.cpp on local repo 1. add printing on local repo v1

--- index.cpp
+++ index.cpp
@@ -4,5 +4,6 @@ int main(){
     std::cout<<"hello Git world !"<<std::endl;
     std::cout<<"Editing on GitHub web"<<std::endl;
     std::cout<<"Edit on GitHub web after git branch --set-upstream-to=jorigin/master master"<<std::endl;
+    std::cout<<"add printing on local repo v1"<<std::endl;
     return 0;
 }
